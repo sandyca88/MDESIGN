3cb73d9e90dc6305afef7b836717d204e059bf05
sandyca88 <tosandy@gmail.com  git config --global user.name sandyca88>
2018-10-16 17:09:00 -0700

    git

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Sandy Chang',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/components/header.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './Header.css'
+
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  handlePurchase = (token) => {
+    const amount = 5000
+    const description = "My awesome product"
+
+    const bodyObject = {
+      tokenId: token.id,
+      email: token.email,
+      name: token.name,
+      description,
+      amount
+    }
+
+    fetch('http://localhost:9000/stripe-charge', {
+      method: 'POST',
+      body: JSON.stringify(bodyObject)
+    })
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src='/images/logo-designcode.svg' width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
